fa36ab284f4c36bfc2490599060189fd7c727ba9
Raincal <cyj94228@gmail.com>
2019-05-17 23:48:56 +0800

    :bug: fix page 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -12,8 +12,8 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1> Not Found </h1>{' '}
-        <p> You just hit a route that doesn & #39;t exist... the sadness.</p>
+        <h1>Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </Layout>
     )
   }
