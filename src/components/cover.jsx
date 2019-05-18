44b01b28847f5df3a30befce13d7dde6c100a05e
Raincal <cyj94228@gmail.com>
2019-05-19 00:10:15 +0800

    :zap: using webp for cover

--- src/components/cover.jsx
+++ src/components/cover.jsx
@@ -6,7 +6,10 @@ const Cover = ({ post }) => {
   return post.cover ? (
     <div className="flex-shrink-0 mb-4 sm:ml-8 sm:mb-0 h-48 sm:h-auto overflow-hidden rounded">
       <Link to={`/post/${post.slug}`}>
-        <img className="w-full sm:w-56 rounded" src={post.cover} alt="Cover" />
+        <picture>
+          <source className="w-full sm:w-56 rounded" type="image/webp" srcSet={`${post.cover}?x-oss-process=image/format,webp`} alt="Cover" />
+          <img className="w-full sm:w-56 rounded" alt="Cover" src={post.cover} />
+        </picture>
       </Link>
     </div>
   ) : null
