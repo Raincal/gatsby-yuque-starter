70ceb417c382657c35bd3a55058765b1cfbde085
Raincal <cyj94228@gmail.com>
2019-05-20 21:41:42 +0800

    :truck: change assets folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,59 +8,61 @@ module.exports = {
       github: 'Raincal'
     },
   },
-  plugins: [{
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      path: `${__dirname}/content/assets`,
-      name: 'assets',
-    },
-  }, {
-    resolve: 'gatsby-source-yuque',
-    options: {
-      login: 'yuque',
-      repo: 'blog',
-      mdNameFormat: 'slug',
-    },
-  },
-  {
-    resolve: 'gatsby-transformer-remark',
-    options: {
-      plugins: [{
-        resolve: 'gatsby-remark-yuque-images',
-        options: {
-          maxWidth: 768,
-        },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static`,
+        name: 'assets',
       },
-      {
-        resolve: 'gatsby-remark-images',
-        options: {
-          maxWidth: 768,
-        },
-      },
-      {
-        resolve: 'gatsby-remark-responsive-iframe',
-        options: {
-          wrapperStyle: 'margin-bottom: 1.0725rem',
-        },
-      },
-      'gatsby-remark-prismjs',
-      'gatsby-remark-copy-linked-files',
-      'gatsby-remark-smartypants',
-      ],
     },
-  },
-  'gatsby-transformer-sharp',
-  'gatsby-plugin-sharp',
-  // {
-  //   resolve: 'gatsby-plugin-google-analytics',
-  //   options: {
-  //     //trackingId: `ADD YOUR TRACKING ID HERE`,
-  //   },
-  // },
-  {
-    resolve: 'gatsby-plugin-feed',
-    options: {
-      query: `
+    {
+      resolve: 'gatsby-source-yuque',
+      options: {
+        login: 'yuque',
+        repo: 'blog',
+        mdNameFormat: 'slug',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [{
+          resolve: 'gatsby-remark-yuque-images',
+          options: {
+            maxWidth: 768,
+          },
+        },
+        {
+          resolve: 'gatsby-remark-images',
+          options: {
+            maxWidth: 768,
+          },
+        },
+        {
+          resolve: 'gatsby-remark-responsive-iframe',
+          options: {
+            wrapperStyle: 'margin-bottom: 1.0725rem',
+          },
+        },
+        'gatsby-remark-prismjs',
+        'gatsby-remark-copy-linked-files',
+        'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     //trackingId: `ADD YOUR TRACKING ID HERE`,
+    //   },
+    // },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
           {
             site {
               siteMetadata {
@@ -72,20 +74,20 @@ module.exports = {
             }
           }
         `,
-      feeds: [
-        {
-          serialize: ({ query: { site, allYuqueDoc } }) => {
-            return allYuqueDoc.edges.map(edge => {
-              return Object.assign({}, edge.node.childMarkdownRemark.frontmatter, {
-                description: edge.node.custom_description,
-                date: edge.node.created_at,
-                url: site.siteMetadata.siteUrl + 'post/' + edge.node.slug,
-                guid: site.siteMetadata.siteUrl + 'post/' + edge.node.slug,
-                custom_elements: [{ 'content:encoded': edge.node.childMarkdownRemark.html }],
+        feeds: [
+          {
+            serialize: ({ query: { site, allYuqueDoc } }) => {
+              return allYuqueDoc.edges.map(edge => {
+                return Object.assign({}, edge.node.childMarkdownRemark.frontmatter, {
+                  description: edge.node.custom_description,
+                  date: edge.node.created_at,
+                  url: site.siteMetadata.siteUrl + 'post/' + edge.node.slug,
+                  guid: site.siteMetadata.siteUrl + 'post/' + edge.node.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.childMarkdownRemark.html }],
+                })
               })
-            })
-          },
-          query: `
+            },
+            query: `
             {
               allYuqueDoc(sort: { fields: [created_at], order: DESC }) {
                 edges {
@@ -104,34 +106,34 @@ module.exports = {
               }
             }
             `,
-          output: '/rss.xml',
-          title: 'Gatsby Yuque Starter',
-        },
-      ],
+            output: '/rss.xml',
+            title: 'Gatsby Yuque Starter',
+          },
+        ],
+      },
     },
-  },
-  {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: 'Gatsby Yuque Starter',
-      short_name: 'Yuque',
-      start_url: '/',
-      background_color: '#ffffff',
-      theme_color: '#48bb78',
-      display: 'minimal-ui',
-      icon: 'content/assets/yuque-icon.png',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Gatsby Yuque Starter',
+        short_name: 'Yuque',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#48bb78',
+        display: 'standalone',
+        icon: 'static/yuque.png',
+      },
     },
-  },
-  'gatsby-plugin-postcss',
-  {
-    resolve: 'gatsby-plugin-purgecss',
-    options: {
-      tailwind: true,
-      whitelist: ['blockquote', 'ol'],
-      purgeOnly: ['src/css/style.css']
-    }
-  },
-  'gatsby-plugin-offline',
-  'gatsby-plugin-react-helmet',
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        tailwind: true,
+        whitelist: ['blockquote', 'ol'],
+        purgeOnly: ['src/css/style.css']
+      }
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
   ],
 }
