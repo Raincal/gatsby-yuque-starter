f1301bd2aeb71213cd1b6d7365d29eb68cc1b61d
Raincal <cyj94228@gmail.com>
2019-05-20 21:16:09 +0800

    :memo: minor update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => (
   <footer className='text-center text-gray-600 pb-6'>
     ©{new Date().getFullYear()}, Built with
-    <a href="https://www.gatsbyjs.org"> Gatsby </a>
+    <a className='text-blue-500' target='_blank' rel="noopener noreferrer" href="https://www.gatsbyjs.org"> Gatsby </a>
   </footer>
 )
 
